a7bd3d356b506c4eb49ca90c9ca0aca802f28ade
GHowlett <griff.howl@gmail.com>
2015-10-25 04:08:32 -0700

    restructured gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,39 @@ var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 
+var src_path = "src/";
+var build_path = "www/";
+
 gulp.task('lint', function(){
-	return gulp.src('src/js/*.js')
+	return gulp.src(src_path + 'js/**/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 	;
 });
 
+gulp.task('copy', function(){
+	return gulp.src([
+		src_path + '**/*.html',
+		src_path + '**/*.css',
+		src_path + '**/*.js'])
+		.pipe(gulp.dest(build_path))
+	;
+});
+
 gulp.task('uglify', function(){
-	return gulp.src('src/js/*.js')
-		.pipe(gulp.dest('www/js'))
+	return gulp.src(build_path + 'js/**/*.js')
+		.pipe(gulp.dest(build_path + 'js_build'))
 		.pipe(uglify())
 	;
 });
 
 gulp.task('csso', function(){
-	return gulp.src('src/css/*.css')
-		.pipe(gulp.dest('www/css'))
+	return gulp.src(build_path + 'css/**/*.css')
+		.pipe(gulp.dest(build_path + 'css'))
 		.pipe(csso())
 	;
 });
 
-gulp.task('copy', function(){
-	return gulp.src('src/*.html')
-		.pipe(gulp.dest('www'))
-	;
-});
-
-gulp.task('default', ['lint', 'uglify', 'csso', 'copy']);
+gulp.task('development', ['lint', 'copy']);
+gulp.task('production', ['lint', 'copy', 'uglify', 'csso']);
+gulp.task('default', ['production']);
