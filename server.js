f14f59b2ef9a7c99b39b910e7f099821d72ea585
GHowlett <griff.howl@gmail.com>
2017-02-27 20:29:34 -0800

    removed bower

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 
 var server = express()
 	.use(express.static(__dirname + '/www'))
-	.use(express.static(__dirname + '/bower_components'))
 
 var port = process.env.PORT || 3000;
 server.listen(port);
